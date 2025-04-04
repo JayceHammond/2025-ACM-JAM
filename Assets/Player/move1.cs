155b83017b36bdfd49d9278a982e53d6e8987a50
cazcheramie123 <cazcheramie@gmail.com>
2025-04-04 14:06:00 -0500

    player movement
    movement in player

---
+++ Assets/Player/move1.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float moveSpeed= 10;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right *moveSpeed *Time.deltaTime);
+
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left *moveSpeed *Time.deltaTime);
+
+        }
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector2.up *moveSpeed *Time.deltaTime);
+
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector2.down *moveSpeed *Time.deltaTime);
+
+        }
+    }
+}
